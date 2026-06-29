33ecb0646721603f6c211d3c764749dc89139e7f
Sohil Desai <sohildesai@usf.edu>
2026-06-28 22:42:48 -0400

    Implement fetchProductsAsync to retrieve products
    Added an asynchronous function to fetch products from an API and display them.

--- cp_2.js
+++ cp_2.js
@@ -41,3 +41,23 @@ function displayProducts(products) {
     });
 
 }
+async function fetchProductsAsync() {
+
+    try {
+
+        const response = await fetch(
+            "https://www.course-api.com/javascript-store-products"
+        );
+
+        const products = await response.json();
+
+        displayProducts(products);
+
+    }
+    catch (error) {
+
+        handleError(error);
+
+    }
+
+}
